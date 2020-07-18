19cc22efe0634f821bfd577598433181db9a92a1
LiaoMingKai <2442104111@qq.com>
2020-07-18 17:50:41 +0800

    测试push

--- BE/src/test/java/com/suncaper/hotelorder/HotelorderApplicationTests.java
+++ BE/src/test/java/com/suncaper/hotelorder/HotelorderApplicationTests.java
@@ -3,6 +3,10 @@ package com.suncaper.hotelorder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+/**
+ * 测试
+ */
 @SpringBootTest
 class HotelorderApplicationTests {
 
