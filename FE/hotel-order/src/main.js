fbcc3e2e851adc5cc9dd13047a87c38658d2c055
田湘龙 <727393818@qq.com>
2020-07-30 19:44:29 +0800

    删除无用文件

--- FE/hotel-order/src/main.js
+++ FE/hotel-order/src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import getList from "./plugin/getdata";
 
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
@@ -10,7 +9,6 @@ import "element-ui/lib/theme-chalk/index.css";
 import "./axios";
 
 Vue.use(ElementUI);
-Vue.use(getList);
 
 Vue.config.productionTip = false;
 
